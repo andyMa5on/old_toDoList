b74aadd1ccd9119d0dc1881cfccd0eae871a642b
Andy <andrew.mason7630@gmail.com>
2021-06-28 19:08:02 +0100

    Update UI & Coding
    Refreshed the User Interface and had a go at re developing the Javascript code.

--- todolist.js
+++ todolist.js
@@ -1,122 +1,119 @@
 // Javascript
+const backDrop = document.getElementById('backdrop');
+const addItemModule = document.getElementById('addItemModule')
+const addButton = document.getElementById('addBttn');
+const closeButton = document.getElementById('closeBttn')
+const ulElement = document.getElementById('userInterface');
+const userInput = document.getElementsByName('input');
+const arrowUp = document.getElementsByName('move_up');
+const ArrowDown = document.getElementsByName('move_down');
 
-function updateList (newItem) {
+const toDoList = []
 
-    if( newItem == ''){
-        alert('Please enter a title')
-        document.getElementById('addItem').focus();
-    } else {
-        const divElem = document.getElementById('listSection');
-        const ulElem = document.getElementById('tdList');
+function addToDoListItemHandler() {
+	let title = userInput[0].value
+	let description = userInput[1].value
 
-        const liEl = document.createElement('li');
+	let newItem = {
+		title: title,
+		description: description
+	}
 
-        const aUp = document.createElement('img');
-        aUp.src = "Images//ArrowUp.png";
-        aUp.setAttribute('id', 'moveUp');
-        aUp.setAttribute('class', 'priorityArrows');
-        aUp.setAttribute('onclick', 'moveFunc(this.id, this.parentElement);');
-        aUp.setAttribute('alt', 'Move Up');
-        liEl.appendChild(aUp);
+	toDoList.push(newItem)
+	createLiElement(newItem.title, newItem.description)
+	userInput[0].value = ''
+	userInput.value = ''
+}
 
-        const checkbox = document.createElement('input')
-        checkbox.setAttribute('type', 'checkbox')
-        checkbox.setAttribute('onchange', 'checkBox(this.checked);')
-        liEl.appendChild(checkbox)
+function createLiElement(title, descrition) {
+	const liEl = document.createElement('li')
+	liEl.innerHTML = `
+	<div class='card toDoCard'>
+		<div class='central'>
+			<img src='Images//ArrowUp.png' alt='Up Arrow' name='move_up' class='arrow_Up'>
+			<input type='text'value=${title} class='titleInput' editable>
+			<br>
+			<img src='Images//ArrowDown.png' alt='Down Arrow' name='move_down' class='arrow_Down'>
+			<textarea class='areaDescrip'>${descrition}</textarea>
+		</div>
+		<div class='flex'>
+		<button class='binButton'><img src='Images/delete_red.png' class='binImage' alt='delete item'></button>
+		</div>
+	</div>
+	`;
+	
+	liEl.querySelector('.arrow_Up').addEventListener('click', function() {moveFunc(this, this.parentElement)})
+	liEl.querySelector('.arrow_Down').addEventListener('click', function() {moveFunc(this, this.parentElement)})
+	liEl.querySelector('.binButton').addEventListener('click', function() {removeLi(this.parentElement.parentElement.parentElement)})
+	ulElement.appendChild(liEl);
+}
 
-        const titleText = document.createTextNode(newItem);
-        const inputTitle = document.createElement('input')
-        inputTitle.setAttribute('value', '')
-        inputTitle.setAttribute('class', 'titleInput')
-        inputTitle.value = titleText.nodeValue
-        liEl.appendChild(inputTitle);
+function updateUI() {
+	const entryText = document.getElementById('entry-text');
+	if (toDoList.length === 0) {
+		entryText.style.display = 'block';
+	} else {
+		entryText.style.display = 'none';
+	}
+}
 
-        const brEl = document.createElement('br');
-        liEl.appendChild(brEl);
+function toggleBackdrop() {
+	backDrop.classList.toggle('backdropVisible');
+}
 
-        const aDown = document.createElement('img');
-        aDown.src = "Images//ArrowDown.png";
-        aDown.setAttribute('id', 'moveDown');
-        aDown.setAttribute('class', 'priorityArrows pDown');
-        aDown.setAttribute('onclick', 'moveFunc(this.id, this.parentElement);');
-        aDown.setAttribute('alt', 'Move Up');
-        liEl.appendChild(aDown);
+function closeHandler() {
+	addModalHandler();
+	userInput[0].value = ''
+	userInput[1].value = ''
+}
 
-        const txtArea = document.createElement('textarea');
-        txtArea.setAttribute('id', 'areaDescrip')
-        txtArea.placeholder = 'Add Description here'
-        liEl.appendChild(txtArea);
-
-        const remove = document.createElement('button');
-        const binTag = document.createElement('img')
-        binTag.src = "Images/delete_red.png"
-        binTag.style.width = "90%"
-        binTag.style.padding = "0.1em"
-        remove.appendChild(binTag);
-        remove.style.maxWidth = "30px"
-        remove.setAttribute("onclick", "removeLi(this.parentElement);")
-        liEl.appendChild(remove);
-
-        ulElem.appendChild(liEl);
-
-        divElem.appendChild(ulElem);
-
-        magic()
-    }
-    
+function updateList(newItem) {
+	if (newItem == '') {
+		alert('Please enter a title');
+		document.getElementById('addItem').focus();
+	} else {
+		addToDoListItemHandler()
+		updateUI()
+		addModalHandler(); 
+	}
 }
 
 function removeLi(i) {
+	let el = i;
+	let confirmDeletion = confirm('Are you sure you want to delete this item ? \n \n Click ok to delete.')
+	if (confirmDeletion == true) {
+		el.remove();
+		updateUI();
+	} else {
+		return
+	}
+}
 
-    let el = i
-    el.remove()
+function addModalHandler() {
+	addItemModule.classList.toggle('hidden');
+	toggleBackdrop();
+	userInput[0].focus()
 
 }
 
-function magic (){
+function moveFunc(element, parent) {
+	i = element.getAttribute('name')
+	parent = parent.parentElement.parentElement
+	console.log(parent)
+	if (i == 'move_down') {
+		let test = parent.nextElementSibling;
+		console.log(test);
 
-    i = document.getElementById('nList').getAttribute('class')
+		test.insertAdjacentElement('afterend', parent);
+	} else {
+		let test = parent.previousElementSibling;
+		console.log(test);
 
-    if( i == 'hidden'){
-        let show = document.getElementById("nList")
-    show.setAttribute('class', 'expand')
-    let addicon = document.getElementById('addIcon')
-    addicon.style.opacity = "50%"
-    document.getElementById('addItem').focus();
-    }else {
-        let collapse = document.getElementById("nList")
-        collapse.setAttribute('class', 'hidden')
-        let addicon = document.getElementById('addIcon')
-        addicon.style.opacity = "100%"
-    }
+		parent.insertAdjacentElement('afterend', test);
+	}
 }
 
-function moveFunc(i, x) {
-
-    if (i == 'moveDown'){
-
-        let test = x.nextElementSibling
-        console.log(test)
-
-        test.insertAdjacentElement('afterend', x)
-    } else {
-
-        let test = x.previousElementSibling
-        console.log(test)
-
-        x.insertAdjacentElement('afterend', test)
-
-    }
-}
-
-function checkBox(i) {
-
-    const exDetails = document.getElementById('exDetails')
-
-    if ( i == true) {
-        exDetails.setAttribute('class', 'expand')
-        exDetails.focus();
-    }else {
-        exDetails.setAttribute('class', 'hidden')
-    }
-}
+// Event listeners
+backDrop.addEventListener('click', closeHandler);
+addButton.addEventListener('click', function() {updateList(addItem.value)})
+closeButton.addEventListener('click', closeHandler)
