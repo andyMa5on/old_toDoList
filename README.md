2f213bd5635a7f8282804bf183ff224f5a8b63b8
Andy <andrew.mason7630@gmail.com>
2022-08-09 18:39:18 +0100

    latest Update

--- README.md
+++ README.md
@@ -1,14 +1,51 @@
 # toDoList
 
-To Do List Project
+Welcome to the `todolist` project. 
 
-The aim of this project was to work with javascript to manipulate the DOM and use basic cookies to save a users to do list.
+## ***Basic insight***
 
-Tasks
- 1, Allow the user to add new items to the list by adding new elements to the DOM
- 2, Allow the user to delete items
- 3, Have the ability to move the each item up and down the list due to it priority
- 4, Create a basic cookie to allow the user to save there list for when they revisit the site
+This is a simple project using `HTML`, `CSS` and `Javascript`. 
+
+This todolist comprises of two fields a title and description. To keep it simple all the data is stored in a cookie so you can always reopen the page locally on your computer to gain access to your list.
+
+Infront of the Title on each task are two arrow buttons. These buttons allow you the ability to move a task up and down the list according to priority.
+
+## ***Using the todolist***
+
+### Creating a task
+When you open the `todolist` page, if there are no tasks currently avaliable you will see a message on the screen saying "Your personal TO DO List!". You can add your first take by using the add `button` in the nav bar. Once you have clicked on this button a simple modal will appear where you can entry the information about your task (Title, Description) and two `buttons` (Cancel, Add).
+
+If you fail to provide a title before clicking the add `button` an error message will appear prompting you to add one.
+
+On completion of the fields in the modal, click the add `button` to add your task.
+
+## Removing a task
+If you add a task that you decide you no longer need you can remove it will a click of a button. 
+
+Each task has its own red Bin button in the bottom right. Once clicked a `prompt` message will appear asking if you are sure you want to delete this item. 
+
+`Note: once an item is deleted it is not retrieable.`
+
+## Adjusting tasks due to priority
+
+When a new task is created it is naturally appended to the bottom of the list.
+
+To move it up and down the list, you can click the up and down arrows. When clicked it will move that task one place either up or down. 
+
+## Saving your tasklist
+
+This tasklist uses cookies to automatically save your list. 
+
+The cookie will update when something is changed from removing a task or moving it up and down your list.
+
+## ***Latest News***
+
+This is now the old version. Work has started on a new one that is created using `OOP`. 
+
+It will contain features like adding projects so you can have multiple lists, and a update UI.
+
+You can check out the new one whilst it is in construction: [to_do_list](https://github.com/andyMa5on/to_do_list)
+
  
 
 
--- todolist.js
+++ todolist.js
@@ -1,30 +1,29 @@
 // Javascript
 const backDrop = document.getElementById('backdrop');
-const addItemModule = document.getElementById('addItemModule')
-const addItemButton = document.getElementById('addIcon')
+const addItemModule = document.getElementById('addItemModule');
+const addItemButton = document.getElementById('addIcon');
 const addButton = document.getElementById('addBttn');
-const closeButton = document.getElementById('closeBttn')
+const closeButton = document.getElementById('closeBttn');
 const ulElement = document.getElementById('userInterface');
 const userInput = document.getElementsByName('input');
 
-let toDoList
+let toDoList;
 
 if (localStorage.key('toDoList') !== null) {
-	toDoList = JSON.parse(localStorage.getItem('toDoList'))
+	toDoList = JSON.parse(localStorage.getItem('toDoList'));
 
-	toDoList.forEach(item => {
-		createLiElement(item.id, item.title, item.description)
-		updateUI()
+	toDoList.forEach((item) => {
+		createLiElement(item.id, item.title, item.description);
+		updateUI();
 	});
 } else {
-	toDoList = []
+	toDoList = [];
 }
 
 function addModalHandler() {
 	addItemModule.classList.toggle('hidden');
 	toggleBackdrop();
-	userInput[0].focus()
-
+	userInput[0].focus();
 }
 
 function validateInput(newItem) {
@@ -32,32 +31,32 @@ function validateInput(newItem) {
 		alert('Please enter a title');
 		document.getElementById('addItem').focus();
 	} else {
-		addToDoListItemHandler()
-		updateUI()
+		addToDoListItemHandler();
+		updateUI();
 		closeHandler();
-		localStorage.setItem('toDoList', JSON.stringify(toDoList)) 
+		localStorage.setItem('toDoList', JSON.stringify(toDoList));
 	}
 }
 
 function addToDoListItemHandler() {
-	let id = Date.now()
-	let title = userInput[0].value
-	let description = userInput[1].value
+	let id = Date.now();
+	let title = userInput[0].value;
+	let description = userInput[1].value;
 
 	let newItem = {
 		id: id,
 		title: title,
-		description: description
-	}
+		description: description,
+	};
 
-	toDoList.push(newItem)
-	createLiElement(newItem.id, newItem.title, newItem.description)
-	userInput[0].value = ''
-	userInput.value = ''
+	toDoList.push(newItem);
+	createLiElement(newItem.id, newItem.title, newItem.description);
+	userInput[0].value = '';
+	userInput.value = '';
 }
 
 function createLiElement(id, title, descrition) {
-	const liEl = document.createElement('li')
+	const liEl = document.createElement('li');
 	liEl.innerHTML = `
 	<div class='card toDoCard'>
 		<div class='central'>
@@ -73,12 +72,29 @@ function createLiElement(id, title, descrition) {
 		</div>
 	</div>
 	`;
-	
-	liEl.querySelector('.titleInput').addEventListener('change', function () {titleHandler(this.value, this.previousElementSibling.value)})
-	liEl.querySelector('.areaDescrip').addEventListener('change', function () {descriptionHandler(this.value, this.parentElement.children[1].value)})
-	liEl.querySelector('.arrow_Up').addEventListener('click', function() {moveFunc(this, this.parentElement, this.nextElementSibling.value)})
-	liEl.querySelector('.arrow_Down').addEventListener('click', function() {moveFunc(this, this.parentElement, this.parentElement.children[1].value)})
-	liEl.querySelector('.binButton').addEventListener('click', function() {removeLi(this.parentElement.parentElement.parentElement, this.parentElement.previousElementSibling.children[1].value)})
+
+	liEl.querySelector('.titleInput').addEventListener('change', function () {
+		titleHandler(this.value, this.previousElementSibling.value);
+	});
+	liEl.querySelector('.areaDescrip').addEventListener('change', function () {
+		descriptionHandler(this.value, this.parentElement.children[1].value);
+	});
+	liEl.querySelector('.arrow_Up').addEventListener('click', function () {
+		moveFunc(this, this.parentElement, this.nextElementSibling.value);
+	});
+	liEl.querySelector('.arrow_Down').addEventListener('click', function () {
+		moveFunc(
+			this,
+			this.parentElement,
+			this.parentElement.children[1].value
+		);
+	});
+	liEl.querySelector('.binButton').addEventListener('click', function () {
+		removeLi(
+			this.parentElement.parentElement.parentElement,
+			this.parentElement.previousElementSibling.children[1].value
+		);
+	});
 	ulElement.appendChild(liEl);
 }
 
@@ -92,19 +108,19 @@ function updateUI() {
 }
 
 function titleHandler(newValue, arrayId) {
-	let idx = findArrayIndex(arrayId)
-	toDoList[idx].title = newValue
+	let idx = findArrayIndex(arrayId);
+	toDoList[idx].title = newValue;
 	refreshStorageArray();
 }
 
 function descriptionHandler(newValue, arrayId) {
-	let idx = findArrayIndex(arrayId)
-	toDoList[idx].description = newValue
+	let idx = findArrayIndex(arrayId);
+	toDoList[idx].description = newValue;
 	refreshStorageArray();
 }
 
-function refreshStorageArray () {
-	localStorage.setItem('toDoList', JSON.stringify(toDoList))
+function refreshStorageArray() {
+	localStorage.setItem('toDoList', JSON.stringify(toDoList));
 }
 
 function toggleBackdrop() {
@@ -114,61 +130,63 @@ function toggleBackdrop() {
 function closeHandler() {
 	addItemModule.classList.toggle('hidden');
 	toggleBackdrop();
-	userInput[0].value = ''
-	userInput[1].value = ''
+	userInput[0].value = '';
+	userInput[1].value = '';
 }
 
 function findArrayIndex(arrayId) {
-	const arrayIndex = toDoList.findIndex((item, idx, list) =>{
-		return item.id == arrayId
-	})
-	return arrayIndex
+	const arrayIndex = toDoList.findIndex((item, idx, list) => {
+		return item.id == arrayId;
+	});
+	return arrayIndex;
 }
 
 function removeLi(i, arrayId) {
-	let idx = findArrayIndex(arrayId)
+	let idx = findArrayIndex(arrayId);
 	let el = i;
-	let confirmDeletion = confirm('Are you sure you want to delete this item ? \n \n Click ok to delete.')
-	
+	let confirmDeletion = confirm(
+		'Are you sure you want to delete this item ? \n \n Click ok to delete.'
+	);
+
 	if (confirmDeletion == true) {
-		toDoList.splice(idx, 1) ;
+		toDoList.splice(idx, 1);
 		el.remove();
 		updateUI();
 		refreshStorageArray();
 	} else {
-		return
+		return;
 	}
-	
 }
 
 function moveFunc(element, liElement, arrayId) {
-	i = element.getAttribute('name')
-	liElement = liElement.parentElement.parentElement
+	i = element.getAttribute('name');
+	liElement = liElement.parentElement.parentElement;
 
-	if ((i == 'move_down') && (liElement.nextElementSibling !== null)) {
-		const arrayIndex = findArrayIndex(arrayId)
-		const item2Move = toDoList[arrayIndex]
-		const swapWith = toDoList[arrayIndex + 1]
-		toDoList[arrayIndex] = swapWith
-		toDoList[arrayIndex + 1] = item2Move
+	if (i == 'move_down' && liElement.nextElementSibling !== null) {
+		const arrayIndex = findArrayIndex(arrayId);
+		const item2Move = toDoList[arrayIndex];
+		const swapWith = toDoList[arrayIndex + 1];
+		toDoList[arrayIndex] = swapWith;
+		toDoList[arrayIndex + 1] = item2Move;
 		let test = liElement.nextElementSibling;
 		test.insertAdjacentElement('afterend', liElement);
 		refreshStorageArray();
-	} else if ((i == 'move_up') && (liElement.previousElementSibling !== null)) {
-		const arrayIndex = findArrayIndex(arrayId)
-		const item2Move = toDoList[arrayIndex]
-		const swapWith = toDoList[arrayIndex - 1]
-		toDoList[arrayIndex] = swapWith
-		toDoList[arrayIndex - 1] = item2Move
+	} else if (i == 'move_up' && liElement.previousElementSibling !== null) {
+		const arrayIndex = findArrayIndex(arrayId);
+		const item2Move = toDoList[arrayIndex];
+		const swapWith = toDoList[arrayIndex - 1];
+		toDoList[arrayIndex] = swapWith;
+		toDoList[arrayIndex - 1] = item2Move;
 		let test = liElement.previousElementSibling;
 		liElement.insertAdjacentElement('afterend', test);
 		refreshStorageArray();
 	}
-
 }
 
 // Event listeners
 backDrop.addEventListener('click', closeHandler);
-addItemButton.addEventListener('click', addModalHandler)
-addButton.addEventListener('click', function() {validateInput(addItem.value)})
-closeButton.addEventListener('click', closeHandler)
+addItemButton.addEventListener('click', addModalHandler);
+addButton.addEventListener('click', function () {
+	validateInput(addItem.value);
+});
+closeButton.addEventListener('click', closeHandler);
